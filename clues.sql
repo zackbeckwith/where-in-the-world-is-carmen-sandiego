82bdad09ff58b2f519e2fc794d4a56fbb77a3b94
zackbeckwith <z.beckwith99@gmail.com>
2022-04-06 18:13:15 -0400

    solve query 1 and 2

--- clues.sql
+++ clues.sql
@@ -2,17 +2,25 @@
 -- description has been traveling through Southern Europe. She's most 
 -- likely traveling someplace where she won't be noticed, so find the least
 -- populated country in Southern Europe, and we'll start looking for her there.
- 
--- TODO: Write SQL query here
 
+--  SELECT 
+-- FROM country 
+-- WHERE region = 'Southern Europe'
+-- ORDER BY population ASC;
+
+-- Vatican
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending 
 -- language classes in this country's officially recognized language. Check our 
 -- databases and find out what language is spoken in this country, so we can 
 -- call in a translator to work with you.
 
--- TODO: Write SQL query here
+-- SELECT language 
+-- FROM countrylanguage 
+-- WHERE isofficial = true 
+-- AND countrycode = 'VAT';
 
+-- Italian
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
@@ -25,7 +33,7 @@
 -- obvious. We're following our gut on this one; find out what other city in 
 -- that country she might be flying to.
 
--- TODO: Write SQL query here
+SELECT * from countrylanguage
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar 
